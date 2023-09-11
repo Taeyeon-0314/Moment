fd301ba68e605ec23472c67a9a3614143558a8a7
Teangoo <lty0314@gmail.com>
2023-09-12 04:25:35 +0900

    feat: LoginResponseDto 클래스에 authToken 필드 추가(JWT토큰 저장용)

--- src/main/java/com/mo/moment/dto/kakaoDto/LoginResponseDto.java
+++ src/main/java/com/mo/moment/dto/kakaoDto/LoginResponseDto.java
@@ -1,10 +1,12 @@
 package com.mo.moment.dto.kakaoDto;
 
 import com.mo.moment.entity.kakaoEntity.KakaoLoginEntity;
+import com.mo.moment.jwt.AuthToken;
 import lombok.Data;
 
 @Data
 public class LoginResponseDto {
     public boolean loginSuccess;
     public KakaoLoginEntity kakaoLoginEntity;
+    public AuthToken authToken;
 }
