fdd7d58c81d563651094b68df7e7ef5d5379f375
Teangoo <lty0314@gmail.com>
2023-09-12 04:22:00 +0900

    chore: 불필요한 import 문 삭제

--- src/main/java/com/mo/moment/controller/KakaoLoginController.java
+++ src/main/java/com/mo/moment/controller/KakaoLoginController.java
@@ -6,11 +6,8 @@ import com.mo.moment.service.kakaoService.KakaoLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
