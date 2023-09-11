5ad05dae58dcda9ab7aba64cf69f9eaa2ef0c66c
Teangoo <lty0314@gmail.com>
2023-09-12 04:16:16 +0900

    feat: AuthRequest 클래스 추가(액세스 토큰 전달)

---
+++ src/main/java/com/mo/moment/jwt/dto/AuthRequest.java
@@ -0,0 +1,12 @@
+package com.mo.moment.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String accessToken;
+}
