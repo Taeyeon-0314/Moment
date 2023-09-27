c7078167fab82139599f133eefd0ad27ea7917e4
Teangoo <lty0314@gmail.com>
2023-09-28 04:09:18 +0900

    feat : 게시판관련사항 기본세팅

---
+++ src/main/java/com/mo/moment/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.mo.moment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+
+//    @GetMapping("/{boardId}")
+//    public ResponseEntity<String> boardView(@PathVariable int boardId){
+//
+//    }
+}
